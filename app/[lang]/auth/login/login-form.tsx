1c644c9570a7019d98f00db14a1205772a77e942
matheusrodacki <44122620+matheusrodacki@users.noreply.github.com>
2024-11-17 15:03:38 -0300

    update auth page links

--- app/[lang]/auth/login/login-form.tsx
+++ app/[lang]/auth/login/login-form.tsx
@@ -1,40 +1,40 @@
-"use client";
-import React from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Loader2 } from "lucide-react";
-import { signIn } from "next-auth/react";
-import toast from "react-hot-toast";
-import { cn } from "@/lib/utils";
-import Link from "next/link";
-import Image from "next/image";
-import { Icon } from "@iconify/react";
-import { Checkbox } from "@/components/ui/checkbox";
-import googleIcon from "@/public/images/auth/google.png";
-import facebook from "@/public/images/auth/facebook.png";
-import twitter from "@/public/images/auth/twitter.png";
-import GithubIcon from "@/public/images/auth/github.png";
-import { SiteLogo } from "@/components/svg";
-import { useMediaQuery } from "@/hooks/use-media-query";
+'use client';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Loader2 } from 'lucide-react';
+import { signIn } from 'next-auth/react';
+import toast from 'react-hot-toast';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Icon } from '@iconify/react';
+import { Checkbox } from '@/components/ui/checkbox';
+import googleIcon from '@/public/images/auth/google.png';
+import facebook from '@/public/images/auth/facebook.png';
+import twitter from '@/public/images/auth/twitter.png';
+import GithubIcon from '@/public/images/auth/github.png';
+import { SiteLogo } from '@/components/svg';
+import { useMediaQuery } from '@/hooks/use-media-query';
 
 const schema = z.object({
-  email: z.string().email({ message: "Your email is invalid." }),
+  email: z.string().email({ message: 'Your email is invalid.' }),
   password: z.string().min(4),
 });
 const LogInForm = () => {
   const [isPending, startTransition] = React.useTransition();
-  const [passwordType, setPasswordType] = React.useState("password");
-  const isDesktop2xl = useMediaQuery("(max-width: 1530px)");
+  const [passwordType, setPasswordType] = React.useState('password');
+  const isDesktop2xl = useMediaQuery('(max-width: 1530px)');
 
   const togglePasswordType = () => {
-    if (passwordType === "text") {
-      setPasswordType("password");
-    } else if (passwordType === "password") {
-      setPasswordType("text");
+    if (passwordType === 'text') {
+      setPasswordType('password');
+    } else if (passwordType === 'password') {
+      setPasswordType('text');
     }
   };
   const {
@@ -44,10 +44,10 @@ const LogInForm = () => {
     formState: { errors },
   } = useForm({
     resolver: zodResolver(schema),
-    mode: "all",
+    mode: 'all',
     defaultValues: {
-      email: "dashtail@codeshaper.net",
-      password: "password",
+      email: 'dashtail@codeshaper.net',
+      password: 'password',
     },
   });
   const [isVisible, setIsVisible] = React.useState(false);
@@ -56,14 +56,14 @@ const LogInForm = () => {
 
   const onSubmit = (data: any) => {
     startTransition(async () => {
-      let response = await signIn("credentials", {
+      let response = await signIn('credentials', {
         email: data.email,
         password: data.password,
         redirect: false,
       });
       if (response?.ok) {
-        toast.success("Login Successful");
-        window.location.assign("/dashboard");
+        toast.success('Login Successful');
+        window.location.assign('/dashboard');
         reset();
       } else if (response?.error) {
         toast.error(response?.error);
@@ -71,166 +71,177 @@ const LogInForm = () => {
     });
   };
   return (
-    <div className="w-full ">
-      <Link href="/dashboard" className="inline-block">
-        <SiteLogo className="h-10 w-10 2xl:w-14 2xl:h-14 text-primary" />
+    <div className='w-full '>
+      <Link href='/dashboard' className='inline-block'>
+        <SiteLogo className='h-10 w-10 2xl:w-14 2xl:h-14 text-primary' />
       </Link>
-      <div className="2xl:mt-8 mt-6 2xl:text-3xl text-2xl font-bold text-default-900">
+      <div className='2xl:mt-8 mt-6 2xl:text-3xl text-2xl font-bold text-default-900'>
         Hey, Hello 👋
       </div>
-      <div className="2xl:text-lg text-base text-default-600 mt-2 leading-6">
+      <div className='2xl:text-lg text-base text-default-600 mt-2 leading-6'>
         Enter the information you entered while registering.
       </div>
-      <form onSubmit={handleSubmit(onSubmit)} className="mt-6 2xl:mt-7">
-        <div className="relative">
+      <form onSubmit={handleSubmit(onSubmit)} className='mt-6 2xl:mt-7'>
+        <div className='relative'>
           <Input
             removeWrapper
-            type="email"
-            id="email"
-            size={!isDesktop2xl ? "xl" : "lg"}
-            placeholder=" "
+            type='email'
+            id='email'
+            size={!isDesktop2xl ? 'xl' : 'lg'}
+            placeholder=' '
             disabled={isPending}
-            {...register("email")}
-            className={cn("peer", {
-              "border-destructive": errors.email,
+            {...register('email')}
+            className={cn('peer', {
+              'border-destructive': errors.email,
             })}
           />
           <Label
-            htmlFor="email"
-            className="rounded-md absolute text-base text-default-600  duration-300 transform -translate-y-5 scale-75 top-2 z-10 origin-[0]   bg-background  px-2 peer-focus:px-2
+            htmlFor='email'
+            className='rounded-md absolute text-base text-default-600  duration-300 transform -translate-y-5 scale-75 top-2 z-10 origin-[0]   bg-background  px-2 peer-focus:px-2
                peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 
-               peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-          >
+               peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1'>
             Email
           </Label>
         </div>
         {errors.email && (
-          <div className=" text-destructive mt-2">{errors.email.message}</div>
+          <div className=' text-destructive mt-2'>{errors.email.message}</div>
         )}
-        <div className="relative mt-6">
+        <div className='relative mt-6'>
           <Input
             removeWrapper
-            type={passwordType === "password" ? "password" : "text"}
-            id="password"
-            size={!isDesktop2xl ? "xl" : "lg"}
-            placeholder=" "
+            type={passwordType === 'password' ? 'password' : 'text'}
+            id='password'
+            size={!isDesktop2xl ? 'xl' : 'lg'}
+            placeholder=' '
             disabled={isPending}
-            {...register("password")}
-            className={cn("peer", {
-              "border-destructive": errors.password,
+            {...register('password')}
+            className={cn('peer', {
+              'border-destructive': errors.password,
             })}
           />
           <Label
-            htmlFor="password"
+            htmlFor='password'
             className={cn(
-              "rounded-md absolute text-base text-default-600  duration-300 transform -translate-y-5 scale-75 top-2 z-10 origin-[0]   bg-background  px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75  peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1",
+              'rounded-md absolute text-base text-default-600  duration-300 transform -translate-y-5 scale-75 top-2 z-10 origin-[0]   bg-background  px-2 peer-focus:px-2 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75  peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1',
               {
-                " text-sm ": isDesktop2xl,
+                ' text-sm ': isDesktop2xl,
               }
-            )}
-          >
+            )}>
             Password
           </Label>
           <div
-            className="absolute top-1/2 -translate-y-1/2 ltr:right-4 rtl:left-4 cursor-pointer"
-            onClick={togglePasswordType}
-          >
-            {passwordType === "password" ? (
-              <Icon icon="heroicons:eye" className="w-4 h-4 text-default-400" />
+            className='absolute top-1/2 -translate-y-1/2 ltr:right-4 rtl:left-4 cursor-pointer'
+            onClick={togglePasswordType}>
+            {passwordType === 'password' ? (
+              <Icon icon='heroicons:eye' className='w-4 h-4 text-default-400' />
             ) : (
               <Icon
-                icon="heroicons:eye-slash"
-                className="w-4 h-4 text-default-400"
+                icon='heroicons:eye-slash'
+                className='w-4 h-4 text-default-400'
               />
             )}
           </div>
         </div>
 
         {errors.password && (
-          <div className=" text-destructive mt-2">
+          <div className=' text-destructive mt-2'>
             {errors.password.message}
           </div>
         )}
 
-        <div className="mt-5  mb-6 flex flex-wrap gap-2">
-          <div className="flex-1 flex  items-center gap-1.5 ">
+        <div className='mt-5  mb-6 flex flex-wrap gap-2'>
+          <div className='flex-1 flex  items-center gap-1.5 '>
             <Checkbox
-              size="sm"
-              className="border-default-300 mt-[1px]"
-              id="isRemebered"
+              size='sm'
+              className='border-default-300 mt-[1px]'
+              id='isRemebered'
             />
             <Label
-              htmlFor="isRemebered"
-              className="text-sm text-default-600 cursor-pointer whitespace-nowrap"
-            >
+              htmlFor='isRemebered'
+              className='text-sm text-default-600 cursor-pointer whitespace-nowrap'>
               Remember me
             </Label>
           </div>
-          <Link href="/auth/forgot4" className="flex-none text-sm text-primary">
+          <Link href='/auth/forgot' className='flex-none text-sm text-primary'>
             Forget Password?
           </Link>
         </div>
         <Button
-          className="w-full"
+          className='w-full'
           disabled={isPending}
-          size={!isDesktop2xl ? "lg" : "md"}
-        >
-          {isPending && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-          {isPending ? "Loading..." : "Sign In"}
+          size={!isDesktop2xl ? 'lg' : 'md'}>
+          {isPending && <Loader2 className='mr-2 h-4 w-4 animate-spin' />}
+          {isPending ? 'Loading...' : 'Sign In'}
         </Button>
       </form>
-      <div className="mt-8 flex flex-wrap justify-center gap-4">
+      <div className='mt-8 flex flex-wrap justify-center gap-4'>
         <Button
-          type="button"
-          size="icon"
-          variant="outline"
-          className="rounded-full  border-default-300 hover:bg-transparent"
+          type='button'
+          size='icon'
+          variant='outline'
+          className='rounded-full  border-default-300 hover:bg-transparent'
           disabled={isPending}
           onClick={() =>
-            signIn("google", {
-              callbackUrl: "/dashboard",
+            signIn('google', {
+              callbackUrl: '/dashboard',
             })
-          }
-        >
-          <Image src={googleIcon} alt="google" className="w-5 h-5" priority={true} />
+          }>
+          <Image
+            src={googleIcon}
+            alt='google'
+            className='w-5 h-5'
+            priority={true}
+          />
         </Button>
         <Button
-          type="button"
-          size="icon"
-          variant="outline"
-          className="rounded-full border-default-300 hover:bg-transparent"
+          type='button'
+          size='icon'
+          variant='outline'
+          className='rounded-full border-default-300 hover:bg-transparent'
           disabled={isPending}
           onClick={() =>
-            signIn("github", {
-              callbackUrl: "/dashboard",
+            signIn('github', {
+              callbackUrl: '/dashboard',
               redirect: false,
             })
-          }
-        >
-          <Image src={GithubIcon} alt="google" className="w-5 h-5" priority={true} />
+          }>
+          <Image
+            src={GithubIcon}
+            alt='google'
+            className='w-5 h-5'
+            priority={true}
+          />
         </Button>
         <Button
-          type="button"
-          size="icon"
-          variant="outline"
-          className="rounded-full  border-default-300 hover:bg-transparent"
-        >
-          <Image src={facebook} alt="google" className="w-5 h-5" priority={true} />
+          type='button'
+          size='icon'
+          variant='outline'
+          className='rounded-full  border-default-300 hover:bg-transparent'>
+          <Image
+            src={facebook}
+            alt='google'
+            className='w-5 h-5'
+            priority={true}
+          />
         </Button>
         <Button
-          type="button"
-          size="icon"
-          variant="outline"
-          className="rounded-full  border-default-300 hover:bg-transparent"
-        >
-          <Image src={twitter} alt="google" className="w-5 h-5" priority={true} />
+          type='button'
+          size='icon'
+          variant='outline'
+          className='rounded-full  border-default-300 hover:bg-transparent'>
+          <Image
+            src={twitter}
+            alt='google'
+            className='w-5 h-5'
+            priority={true}
+          />
         </Button>
       </div>
-      <div className="mt-6 text-center text-base text-default-600">
-        Don't have an account?{" "}
-        <Link href="/auth/register4" className="text-primary">
-          {" "}
-          Sign Up{" "}
+      <div className='mt-6 text-center text-base text-default-600'>
+        Don't have an account?{' '}
+        <Link href='/auth/register' className='text-primary'>
+          {' '}
+          Sign Up{' '}
         </Link>
       </div>
     </div>
--- app/[lang]/auth/register/reg-form.tsx
+++ app/[lang]/auth/register/reg-form.tsx
@@ -239,7 +239,7 @@ const RegForm = () => {
       </div>
       <div className='mt-5 2xl:mt-8 text-center text-base text-default-600'>
         Already Registered?{' '}
-        <Link href='/auth/login4' className='text-primary'>
+        <Link href='/auth/login' className='text-primary'>
           {' '}
           Sign In
         </Link>
